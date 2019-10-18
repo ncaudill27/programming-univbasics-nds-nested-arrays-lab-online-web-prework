c3ce0468b10ac84f142733571862082427f3da52
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-10-18 02:28:13 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -29,6 +29,7 @@ def array_literal_matrix
     ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
     ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
   ]
+  literal_a.sorts
 end
 
 def matrix_lookup(matrix, row, column)
